6bbbef8596fc19f6a89765f32f6833e05d53335b
magdieline <magdieline@hotmail.com>
2022-09-14 16:07:10 -0300

    fix - separando pastas pra  organizar codigo

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const Hapi = require('@hapi/hapi');
+const server = require ('./server')
+
 
 const init = async () => {
-
-    // criando uma instancia do servidor
-
-    const server = Hapi.server ({      //especificando uma conecção(interagir com o servidor)=>informaçõe de conecção
-        port: 8080, 
-        host: 'localhost'             //server.connection ({port:8080, host: '0.0.0.0' }) 
-    //porta 8080 -  atraves do zero. zero ele vai escutar em todas as interfaces de rede do computador
-    }); 
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
-
-
-};
-
-process.on ('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+    try {
+        const serverInstance = await server;
+    
+        await serverInstance.start();
+        console.log('Server running on %s', serverInstance.info.uri);
+        
+      } catch (error) {
+        console.log('message: application startup error');
+        console.log(error.message);
+      }
+    };
+    init();
 
